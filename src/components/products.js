86b79ef3b3e8dd324bd3655e39f4d550c27ee40d
Adam Lang <adam.lang-1@hotmail.com>
2021-04-18 15:06:45 +0200

    feat: fetching json format, displaying products and name in list

--- src/components/products.js
+++ src/components/products.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 const Products = () => {
-    const [product, setProducts] = useState('');
+    const [product, setProducts] = useState([]);
 
     const fetchProducts = async () => {
         const response = await fetch('http://localhost:5000/products');
-        const responseText = await response.text();
+        const responseText = await response.json();
         setProducts(responseText);
         console.log(responseText);
     };
@@ -14,8 +14,15 @@ const Products = () => {
         fetchProducts();
     }, []);
     return (
-        <div>
-            <p>{product}</p>
+        <div className="products">
+            {product.map((product, index) => (
+                <li key={index}>
+                    <a href={product.image}>
+                        <img src={product.image} alt='media' />
+                    </a>
+                    <h2>{product.name}</h2>
+                </li>
+            ))}
         </div>
     );
 };
